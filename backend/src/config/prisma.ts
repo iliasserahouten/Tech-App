f4ab628c8d76a942c69ecc6740e4eaa1e80ba337
ZaouiAnass <anasszaoui123@gmail.com>
2026-03-19 09:45:03 +0100

    migration de prisma 6.19 -> 7.4.0

--- backend/src/config/prisma.ts
+++ backend/src/config/prisma.ts
@@ -1,9 +1,7 @@
+import "dotenv/config";
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { Pool } from "pg";
-import dotenv from "dotenv";
-
-dotenv.config();
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
